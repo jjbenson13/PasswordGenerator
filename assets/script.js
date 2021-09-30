a125a1e625d7fca3705246472b095f56219d3e83
Jasmine Benson <jjortega2010@gmail.com>
2021-09-29 21:43:58 -0700

    updated ReadMe

--- assets/script.js
+++ assets/script.js
@@ -5,26 +5,64 @@ const upperEl = document.getElementById('upper');
 const lowerEl = document.getElementById('lower');
 const numberEl = document.getElementById('number');
 const symbolEl = document.getElementById('lower');
-const generatePasswordE1 = document.getElementById('generatePassword');
+const generateE1 = document.getElementById('generate');
 
-const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const lower = 'abcdefghijklmnopqrstuvwxyz';
-const number = '0123456789';
-const symbol = '!@#$%^&*()_+=';
+const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*()_+=';
 
-function getlower() {
-  return lower[Math.floor(Math.random() * lower.length)]
+function getLowercase() {
+  return lowerLetters[Math.floor(Math.random() * lowerLetters.length)]
 }
 
-function geupper() {
-  return upper[Math.floor(Math.random() * upper.length)]
+function getUppercase() {
+  return upperLetters[Math.floor(Math.random() * upperLetters.length)]
 }
 
-function getnumber() {
-  return number[Math.floor(Math.random() * number.length)]
+function getNumber() {
+  return numbers[Math.floor(Math.random() * numbers.length)]
 }
 
-function getsymbol() {
-  return symbol[Math.floor(Math.random() * symbol.length)]
+function getSymbol() {
+  return symbols[Math.floor(Math.random() * symbols.length)]
 }
 
+function generatePassword() {
+  const length = lengthEl.value;
+
+  let password = '';
+
+  for (let i = 0; i<length; i++) {
+    const x = generatePassword
+    password += x;
+  }
+
+  generateX(){
+    const xs = [];
+    if (upperEl.checked) {
+      xs.push(getUppecase())
+    }
+
+    if (lowerEl.checked) {
+      xs.push(getLowercase())
+    }
+
+    if (numberEl.checked) {
+      xs.push(getNumber())
+    }
+
+    if (symbolEl.checked) {
+      xs.push(getSymbol))
+    }
+
+    return xs[Math.floor(Math.random() * xs.length)];
+  }
+
+
+  pwEl.innerText = password;
+}
+
+function
+
+  generatePasswordE1.addEventListener('click' generatePassword)
