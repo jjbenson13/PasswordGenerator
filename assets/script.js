55c86a8bf9a977c571a87f4a996bb567fcdc4813
Jasmine Benson <jjortega2010@gmail.com>
2021-09-29 21:02:41 -0700

    got some js working

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,30 @@
-const pw = document.getElementById('pw');
-const copy = document.getElementById('copy');
-const length = document.getElementById('length');
-const upper = document.getElementById('upper');
-const lower = document.getElementById('lower');
-const number = document.getElementById('number');
-const symbol = document.getElementById('lower');
-const generatePassword = document.getElementById('generatePassword');
+const pwEl = document.getElementById('pw');
+const copyEl = document.getElementById('copy');
+const lengthEl = document.getElementById('length');
+const upperEl = document.getElementById('upper');
+const lowerEl = document.getElementById('lower');
+const numberEl = document.getElementById('number');
+const symbolEl = document.getElementById('lower');
+const generatePasswordE1 = document.getElementById('generatePassword');
+
+const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lower = 'abcdefghijklmnopqrstuvwxyz';
+const number = '0123456789';
+const symbol = '!@#$%^&*()_+=';
+
+function getlower() {
+  return lower[Math.floor(Math.random() * lower.length)]
+}
+
+function geupper() {
+  return upper[Math.floor(Math.random() * upper.length)]
+}
+
+function getnumber() {
+  return number[Math.floor(Math.random() * number.length)]
+}
+
+function getsymbol() {
+  return symbol[Math.floor(Math.random() * symbol.length)]
+}
+
